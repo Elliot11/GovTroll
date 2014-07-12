d2945a9e0b58cc56bc340380259bee35a00c31bb
Martin Feckie <mfeckie@gmail.com>
2014-07-12 13:18:04 +0800

    Tweak VCR setup

--- spec/lib/search_spec.rb
+++ spec/lib/search_spec.rb
@@ -1,10 +1,4 @@
 require 'spec_helper'
-require 'vcr'
-
-VCR.configure do |config|
-  config.cassette_library_dir = 'fixtures/vcr_cassettes'
-  config.hook_into :webmock
-end
 
 describe 'Search representatives' do
   it 'gets a lists of representative' do
