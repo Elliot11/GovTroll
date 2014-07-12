e8ec0d1298af0aed777b8c18866a6f589c482e7c
Elliot11 <elliot.khoo@gmail.com>
2014-07-12 18:55:01 +0800

    added posts and targets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140712101502) do
+ActiveRecord::Schema.define(version: 20140712105040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,20 @@ ActiveRecord::Schema.define(version: 20140712101502) do
     t.integer  "user_id"
     t.string   "provider"
     t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "targets", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "politician_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
