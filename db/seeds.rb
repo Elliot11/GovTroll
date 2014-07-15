df5c7355371ed66ae7b8d3a3d00fd36f0c7b7e16
Martin Feckie <mfeckie@gmail.com>
2014-07-15 22:27:16 +0800

    Make db:seed for Contact's idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,10 @@ require 'csv'
 file = Rails.root.join('db', 'mp.csv')
 
 CSV.foreach(file, headers: true) do |line|
+  create_new_contact(line) unless Contact.where(first_name: line['first_name'], last_name: line['last_name']).size >= 1
+end
+
+def create_new_contact(line)
   contact            = Contact.new
   contact.first_name = line['first_name']
   contact.last_name  = line['last_name']
