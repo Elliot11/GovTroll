d94d605ea4ab865c018344edd1280b504cb5e41b
Martin Feckie <mfeckie@gmail.com>
2014-07-16 20:46:23 +0800

    refactor of contact decorator

--- app/decorators/contact_decorator.rb
+++ app/decorators/contact_decorator.rb
@@ -3,48 +3,24 @@ class ContactDecorator < Draper::Decorator
   delegate_all
 
   def email
-    wrap_with_icon model.email, 'email', 'envelope-square'
+    wrap_with_icon model.email, 'envelope-square'
   end
 
   def twitter
-    wrap_with_icon model.twitter, 'twitter', 'twitter-square'
+    wrap_with_icon model.twitter,'twitter-square'
   end
 
   def facebook
-    wrap_with_icon model.facebook, 'facebook', 'facebook-square'
+    wrap_with_icon model.facebook, 'facebook-square'
   end
 
   private
 
-  def email_link(property)
-    if property.present?
-      content_tag :span, property
-    else
-      content_tag :span, "No email address found"
-    end
-  end
-
-  def twitter_link(property)
-    if property.present?
-      content_tag :span, property
-    else
-      content_tag :span, "No twitter found"
-    end
-  end
-
-  def facebook_link(property)
-    if property.present?
-      content_tag :span, property
-    else
-      content_tag :span, "No Facebook information found"
-    end
-  end
-
-  def wrap_with_icon(property, title, fa_icon)
+  def wrap_with_icon(property, fa_icon)
     if property.present?
       fa_icon("#{fa_icon} 2x fw") + content_tag(:span, property)
     else
-      fa_icon("#{fa_icon} 2x fw") + content_tag(:span, "No #{title} available")
+      fa_icon("#{fa_icon} 2x fw", class: 'text-danger') + content_tag(:span, "unavailable")
     end
   end
 
