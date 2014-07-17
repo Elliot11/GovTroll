02a530381342821892351bc1fe0518e89ad83ef9
Elliot11 <elliot.khoo@gmail.com>
2014-07-17 19:34:00 +0800

    moved create_new_contact to top of page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
 require 'csv'
 
-file = Rails.root.join('db', 'mp.csv')
-
-CSV.foreach(file, headers: true) do |line|
-  create_new_contact(line) unless Contact.where(first_name: line['first_name'], last_name: line['last_name']).size >= 1
-end
-
 def create_new_contact(line)
   contact            = Contact.new
   contact.first_name = line['first_name']
@@ -14,4 +8,10 @@ def create_new_contact(line)
   contact.twitter = line['twitter'].split('/').last unless line['twitter'].nil?
   contact.facebook = line['facebook']
   contact.save
+end
+
+file = Rails.root.join('db', 'mp.csv')
+
+CSV.foreach(file, headers: true) do |line|
+  create_new_contact(line) unless Contact.where(first_name: line['first_name'], last_name: line['last_name']).size >= 1
 end
