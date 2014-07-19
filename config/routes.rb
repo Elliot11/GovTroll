9f760dd68cbafe3e5c85184c5e71863b7f36bf66
Rogério R. Alcântara <rogerio.alcantara@avatek.com.br>
2014-07-19 18:37:01 +0800

    Integrating post troll

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
 
   resources :posts
   post 'post/modal', to:'posts#modal', as: :post_modal
+  post 'post/modal/create', to:'posts#troll', as: :post_troll
 
   get 'search', to: 'search#index'
   get 'search/results', to: 'search#results'
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
              controllers: {omniauth_callbacks: "omniauth_callbacks"}
-
 
   get 'guide', to: 'home#guide'
   get 'video', to: 'home#video'
