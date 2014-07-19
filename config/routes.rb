09208c4738e46383de6dd50627861d37e29f1d49
Martin Feckie <mfeckie@gmail.com>
2014-07-19 18:22:05 +0800

    Add CORS support to API

--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,24 @@ Rails.application.routes.draw do
   resources :contacts
 
   resources :posts
-  post 'post/modal', to:'posts#modal', as: :post_modal
+  post 'post/modal', to: 'posts#modal', as: :post_modal
 
   get 'search', to: 'search#index'
   get 'search/results', to: 'search#results'
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
-             controllers: {omniauth_callbacks: "omniauth_callbacks"}
+             controllers:        {omniauth_callbacks: "omniauth_callbacks"}
 
 
   get 'guide', to: 'home#guide'
   get 'video', to: 'home#video'
   root 'home#index'
 
-  get 'sign_up/modal', to:'home#sign_up_modal', as: :sign_up_modal
+  get 'sign_up/modal', to: 'home#sign_up_modal', as: :sign_up_modal
 
   namespace :api do
     namespace :v1 do
-      namespace :open_australia_proxy do
+      namespace :open_australia_proxy, defaults: {format: :json} do
         get :representatives
       end
     end
